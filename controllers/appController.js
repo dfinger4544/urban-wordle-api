c6b77293b684c31b74745bfb0c75d7dde430d569
Daniel Finger <dfinger4544@gmail.com>
2024-01-11 18:23:55 -0600

    Working Commit

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,3 +1,10 @@
+const Solution = require("../models/solutionModel");
+
 module.exports.getTest = async (req, res) => {
-    res.render('index')
-}
+  const solution = await Solution.getTodaysWord();
+  const masked_word = solution.word.toUpperCase().replace(/[A-Z]/g, "x");
+
+  res.render("index", {
+    masked_word,
+  });
+};
